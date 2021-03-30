dac583ebfa8258c06644f98c4003a3ea9eaa878e
learninglegion <brent.beckwith@gmail.com>
2021-03-30 21:32:54 +0000

    Update 1 20210330

---
+++ playalongchapter6.py
@@ -0,0 +1,9 @@
+alien_0 = {'color': 'green', 'points': 5}
+# print(alien_0['color'])
+# print(alien_0['points'])
+new_points = alien_0['points']
+print(f"You just shot down a {alien_0['color']} alien! You earned {new_points} points!")
+
+
+# aliens = {[{alien_0 = 'color': 'green', 'points': 5},{alien_1 = 'color': 'yellow', 'points': 10},{alien_2 = 'color': 'red', 'points': 15}]}
+# print(aliens{alien_2['color']})
