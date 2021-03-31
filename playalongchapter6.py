2216cceb3887f3089080c51a9009eebfe0212f00
learninglegion <brent.beckwith@gmail.com>
2021-03-31 21:27:41 +0000

    update-2021033101

--- playalongchapter6.py
+++ playalongchapter6.py
@@ -51,14 +51,26 @@ favorite_languages = {
 # point_value = alien_0.get('points', 'No point value assigned.')
 # print(point_value)
 
-user_0 = {
-    'username': 'efermi',
-    'first': 'enrico',
-    'last': 'fermi',
-}
-for key, value in user_0.items():
-    print(f"\nKey: {key}")
-    print(f"Value: {value}")
-for key, value in favorite_languages.items():
-    print(f"\nName: {key.title()}")
-    print(f"Language: {value.title()}")
+# user_0 = {
+#     'username': 'efermi',
+#     'first': 'enrico',
+#     'last': 'fermi',
+# }
+# for key, value in user_0.items():
+#     print(f"\nKey: {key}")
+#     print(f"Value: {value}")
+# for name, language in favorite_languages.items():
+#     print(f"{name.title()}'s favorite language is {language.title()}!")
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(f"Hi, {name.title()}.")
+    if name in friends:
+#        language = favorite_languages[name].title()
+#        print(f"{name.title()}, I see you love {language}!")
+        print(f"{name.title()}, I see you love {favorite_languages[name]}!")
+if 'erin' not in favorite_languages.keys():
+    print(f"Erin, please take our poll.")
+
+
+
+
