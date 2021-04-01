7db0d43b5dc6db59823454029fd74c7a8bc7d299
learninglegion <brent.beckwith@gmail.com>
2021-04-01 23:34:37 +0000

    update2020040101

--- playalongchapter6.py
+++ playalongchapter6.py
@@ -86,25 +86,70 @@
 # for alien in aliens:
 #     print(alien)
 
-#Make an empty list for storing aliens
-aliens = []
-#Make 30 green aliens
-for alien in range(30):
-    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
-    aliens.append(new_alien)
-for alien in aliens[:3]:
-    if alien['color'] == 'green':
-        alien['color'] = 'yellow'
-        alien['points'] = 10
-        alien['speed'] = 'medium'
-    elif alien['color'] == 'yellow':
-        alien['color'] == 'red':
-        alien['points'] == 15
-        alien['speed'] == 'fast'
-#Show the first 5 aliens
-for alien in aliens[:5]:
-    print(alien)
-print('...')
-#Show how many aliens have been created
-print(f"Total number of aliens: {len(aliens)}")
+#dictionary in list
+# #Make an empty list for storing aliens
+# aliens = []
+# #Make 30 green aliens
+# for alien in range(30):
+#     new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+#     aliens.append(new_alien)
+# for alien in aliens[:3]:
+#     if alien['color'] == 'green':
+#         alien['color'] = 'yellow'
+#         alien['points'] = 10
+#         alien['speed'] = 'medium'
+#     elif alien['color'] == 'yellow':
+#         alien['color'] == 'red':
+#         alien['points'] == 15
+#         alien['speed'] == 'fast'
+# #Show the first 5 aliens
+# for alien in aliens[:5]:
+#     print(alien)
+# print('...')
+# #Show how many aliens have been created
+# print(f"Total number of aliens: {len(aliens)}")
 
+#list in dictionary
+#Store info about the pizza being ordered
+# pizza = {
+#     'crust': 'thick',
+#     'toppings': ['mushrooms', 'extra cheese'],
+# }
+# #Summarize the order
+# print(f"Hey I got your {pizza['crust']} crust pizza here "
+#     "with the following toppings:")
+# for topping in pizza['toppings']:
+#     print("\t" + topping.title())
+
+#list in dictionary
+# favorite_languages = {
+#     'jen': ['python', 'ruby'],
+#     'sarah': 'c',
+#     'edward': ['ruby', 'go'],
+#     'phil': ['python', 'haskell'],
+#     'alex': ['javascript','php'],
+# }
+# for name, languages in favorite_languages.items():
+#     print(f"\n{name.title()}'s favorite languages are:")
+#     for language in languages:
+#         print(f"\t{language.title()}")
+
+#dictionary in dictionary
+# users = {
+#     'aeinstein': {
+#         'first': 'albert',
+#         'last': 'einstein',
+#         'location': 'princeton',
+#     },
+#     'mcurie': {
+#         'first': 'marie',
+#         'last': 'curie',
+#         'location': 'paris',
+#     }
+# }
+# for username, user_info in users.items():
+#     print(f"\nUsername: {username}")
+#     full_name = f"{user_info['first']} {user_info['last']}"
+#     location = user_info['location']
+#     print(f"\tFull name: {full_name.title()}")
+#     print(f"\tLocation: {location.title()}")
