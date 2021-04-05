386fdfb6ec9e73250d6c57f92737f00e5de915fd
learninglegion <brent.beckwith@gmail.com>
2021-04-05 18:24:35 +0000

    update2020040501

---
+++ PAchap7.py
@@ -0,0 +1,28 @@
+#parrot
+# message = input("Tell me something and I will repeat it back to you: ")
+# print(message)
+
+#greeter
+# name = input("Please enter your name: ")
+# print(f"\nHello, {name}!")
+
+# prompt = "If you tell us who you are, we can personalize the messages you see."
+# prompt += "\nWhat is your first name? "
+# name = input(prompt)
+# print(f"\nHello, {name}!")
+
+#rollercoaster
+# height = input("How tall are you, in inches? ")
+# height = int(height)
+# if height >= 48:
+#     print("You're tall enough to ride this ride!")
+# else:
+#     print("\nYou'll be able to ride when you're a little taller.")
+
+#even_or_odd
+number = input("Enter a number and I I'll tell you if it's even or odd: ")
+number = int(number)
+if number % 2 == 0:
+    print(f"The number {number} is even.")
+else:
+    print(f"\nThe number {number} is odd.")
