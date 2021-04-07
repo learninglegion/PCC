a3f1e3dadaac83bd9e179ad61642bb914b8dfed3
learninglegion <brent.beckwith@gmail.com>
2021-04-07 22:10:42 +0000

    update2021040701

--- PAchap8.py
+++ PAchap8.py
@@ -4,7 +4,28 @@
 #     print("Hello!")
 # greet_user()
 #
-def greet_user(username):
-    """Display a simple greeting."""
-    print(f"Hello, {username.title()}!")
-greet_user('jesse')
+# def greet_user(username):
+#     """Display a simple greeting."""
+#     print(f"Hello, {username.title()}!")
+# greet_user('jesse')
+
+#pets
+# def describe_pet(animal_type, pet_name):
+#     """Display information about a pet."""
+#     print(f"\nI have a {animal_type}.")
+#     print(f"My {animal_type}'s name is {pet_name.title()}.")
+# describe_pet('hamster','harry')
+# describe_pet('dog','willie')
+#
+# def describe_pet(animal_type, pet_name):
+#     """Display information about a pet."""
+#     print(f"\nI have a {animal_type}.")
+#     print(f"My {animal_type}'s name is {pet_name.title()}.")
+# describe_pet(pet_name='harry', animal_type='hamster')
+#
+def describe_pet(pet_name, animal_type='dog'):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+#describe_pet('willie')
+describe_pet(pet_name='harry', animal_type='hamster')
--- chap8exercises.py
+++ chap8exercises.py
@@ -5,7 +5,32 @@
 # display_message()
 
 #8.2
-def favorite_book(title):
-    """Print the title of one of my favorite books."""
-    print(f"One of my favorite books is {title.title()}.")
-favorite_book('the wastelands')
+# def favorite_book(title):
+#     """Print the title of one of my favorite books."""
+#     print(f"One of my favorite books is {title.title()}.")
+# favorite_book('the wastelands')
+
+#8.3
+# def make_shirt(size, text):
+#     """Get parameters for a shirt to make."""
+#     print(f"Ayo we need a {size} shirt with '{text.title()}' on it ASAP!")
+# #make_shirt('medium', 'you can certainly try.')
+# make_shirt(size='medium', text='you can certainly try.')
+
+#8.4
+# def make_shirt(size='large', text='I love Python.'):
+#     """Get parameters for a shirt to make."""
+#     print(f"Ayo we need a {size} shirt with '{text}' on it ASAP!")
+# make_shirt()
+# make_shirt(size='medium', text='You can certainly try.')
+
+#8.5
+def describe_city(name, country='china'):
+    """List the name of a city and the country it's in."""
+    if country == 'usa' or country == 'united states':
+        print(f"{name.title()} is a city in the {country.title()}.")
+    else:
+        print(f"{name.title()} is a city in {country.title()}.")
+describe_city('beijing')
+describe_city(name='detroit', country='united states')
+describe_city(country='brazil', name='Rio')
