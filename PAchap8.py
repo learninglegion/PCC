56b3567e6696dd8898bac1829f3d3be696ff171f
learninglegion <brent.beckwith@gmail.com>
2021-04-07 21:39:03 +0000

    update2021040700

---
+++ PAchap8.py
@@ -0,0 +1,10 @@
+#greeter
+# def greet_user():
+#     """Display a simple greeting."""
+#     print("Hello!")
+# greet_user()
+#
+def greet_user(username):
+    """Display a simple greeting."""
+    print(f"Hello, {username.title()}!")
+greet_user('jesse')
---
+++ chap8exercises.py
@@ -0,0 +1,11 @@
+#8.1
+# def display_message():
+#     """Print one sentence about the material."""
+#     print("I'm learning about functions. They're neat.")
+# display_message()
+
+#8.2
+def favorite_book(title):
+    """Print the title of one of my favorite books."""
+    print(f"One of my favorite books is {title.title()}.")
+favorite_book('the wastelands')
