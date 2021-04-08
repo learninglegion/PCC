b27106f5e82deee4e01883cabeaaefb8e62163f4
learninglegion <brent.beckwith@gmail.com>
2021-04-08 23:29:53 +0000

    update2021040800

--- PAchap8.py
+++ PAchap8.py
@@ -60,18 +60,49 @@
 # print(musician)
 
 #"greeter"
-def get_formatted_name(first_name, last_name):
-    """Return a full name, neatly formatted."""
-    full_name = f"{first_name} {last_name}"
-    return full_name.title()
-while True:
-    print("\nPlease tell me your name:")
-    print("(Enter 'q' at any time to quit)")
-    f_name = input("First name: ")
-    if f_name == 'q':
-        break
-    l_name = input("Last name: ")
-    if l_name == 'q':
-        break
-    formatted_name = get_formatted_name(f_name, l_name)
-    print(f"\nHello, {formatted_name.title()}")
+# def get_formatted_name(first_name, last_name):
+#     """Return a full name, neatly formatted."""
+#     full_name = f"{first_name} {last_name}"
+#     return full_name.title()
+# while True:
+#     print("\nPlease tell me your name:")
+#     print("(Enter 'q' at any time to quit)")
+#     f_name = input("First name: ")
+#     if f_name == 'q':
+#         break
+#     l_name = input("Last name: ")
+#     if l_name == 'q':
+#         break
+#     formatted_name = get_formatted_name(f_name, l_name)
+#     print(f"\nHello, {formatted_name.title()}")
+
+#greet_users
+# def greet_users(names):
+#     """Print a simple greeting to each user in the list."""
+#     for name in names:
+#         msg = f"Hello, {name.title()}!"
+#         print(msg)
+# usernames = ['hannah', 'ty', 'margot']
+# greet_users(usernames)
+
+#printing_models
+#Start with some designs that need to be printed.
+unprinted_designs = ['phone case','robot pendant','dodecahedron']
+completed_models = []
+def print_models(unprinted_designs, completed_models):
+    """
+    Simulate printing each design until none are left.
+    Move each design to completed_models after printing.
+    """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f"Printing model: {current_design}")
+        completed_models.append(current_design)
+def show_completed_models(completed_models):
+    """Show all the models that were printed"""
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+print_models(unprinted_designs[:], completed_models)
+show_completed_models(completed_models)
+print(unprinted_designs)
