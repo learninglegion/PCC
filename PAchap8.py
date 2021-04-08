fe9b776fd30d48d8981630709b7c3243d200687c
learninglegion <brent.beckwith@gmail.com>
2021-04-08 00:05:36 +0000

    update2021040703

--- PAchap8.py
+++ PAchap8.py
@@ -23,9 +23,55 @@
 #     print(f"My {animal_type}'s name is {pet_name.title()}.")
 # describe_pet(pet_name='harry', animal_type='hamster')
 #
-def describe_pet(pet_name, animal_type='dog'):
-    """Display information about a pet."""
-    print(f"\nI have a {animal_type}.")
-    print(f"My {animal_type}'s name is {pet_name.title()}.")
-#describe_pet('willie')
-describe_pet(pet_name='harry', animal_type='hamster')
+# def describe_pet(pet_name, animal_type='dog'):
+#     """Display information about a pet."""
+#     print(f"\nI have a {animal_type}.")
+#     print(f"My {animal_type}'s name is {pet_name.title()}.")
+# #describe_pet('willie')
+# describe_pet(pet_name='harry', animal_type='hamster')
+
+#formatted_name
+# def get_formatted_name(first_name, last_name):
+#     """Return a full name, neatly formatted."""
+#     full_name = f"{first_name} {last_name}"
+#     return full_name.title()
+# musician = get_formatted_name('jimi', 'hendrix')
+# print(musician)
+# def get_formatted_name(first_name, last_name, middle_name=''):
+#     """Return a full name, neatly formatted."""
+#     if middle_name:
+#         full_name = f"{first_name} {middle_name} {last_name}"
+#     else:
+#         full_name = f"{first_name} {last_name}"
+#     return full_name.title()
+# musician = get_formatted_name('john', 'hooker', 'lee')
+# print(musician)
+# musician = get_formatted_name('jimi', 'hendrix')
+# print(musician)
+
+#person
+# def build_person(first_name, last_name, age=None):
+#     """Return a dictionary of information about a person."""
+#     person = {'first': first_name, 'last': last_name}
+#     if age:
+#         person['age'] = age
+#     return person
+# musician = build_person('jimi','hendrix', 27)
+# print(musician)
+
+#"greeter"
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+while True:
+    print("\nPlease tell me your name:")
+    print("(Enter 'q' at any time to quit)")
+    f_name = input("First name: ")
+    if f_name == 'q':
+        break
+    l_name = input("Last name: ")
+    if l_name == 'q':
+        break
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name.title()}")
--- chap8exercises.py
+++ chap8exercises.py
@@ -25,12 +25,53 @@
 # make_shirt(size='medium', text='You can certainly try.')
 
 #8.5
-def describe_city(name, country='china'):
-    """List the name of a city and the country it's in."""
-    if country == 'usa' or country == 'united states':
-        print(f"{name.title()} is a city in the {country.title()}.")
-    else:
-        print(f"{name.title()} is a city in {country.title()}.")
-describe_city('beijing')
-describe_city(name='detroit', country='united states')
-describe_city(country='brazil', name='Rio')
+# def describe_city(name, country='china'):
+#     """List the name of a city and the country it's in."""
+#     if country == 'usa' or country == 'united states':
+#         print(f"{name.title()} is a city in the {country.title()}.")
+#     else:
+#         print(f"{name.title()} is a city in {country.title()}.")
+# describe_city('beijing')
+# describe_city(name='detroit', country='united states')
+# describe_city(country='brazil', name='Rio')
+
+#8.6
+# def city_country(city, country):
+#     """Write a city and country."""
+#     location = f"{city}, {country}"
+#     return location.title()
+# print(city_country('evansville', 'USA'))
+# place = city_country('indianapolis', 'USA')
+# print(place)
+# place = city_country('vincennes', 'USA')
+# print(place)
+
+#8.7
+# def make_album(name, album):
+#     """Describe a music album."""
+#     record = {'artist_name': name.title(), 'artist_album': album.title()}
+#     return record
+# mjk = make_album('tool', 'lateralis')
+# bh = make_album('a perfect circle', '13th step')
+# tr = make_album('nin', 'broken')
+# print(mjk)
+# print(bh)
+# print(tr)
+
+#8.8
+def make_album(name, album):
+    """Describe a music album."""
+    record = {'artist_name': name.title(), 'artist_album': album.title()}
+    return record
+while True:
+    print("\nName some of your favorite musical artists and albums.")
+    print("(Type 'q' to quit entering info.)\n")
+    given_name = input("First, give me the name of a band or artist: ")
+    if given_name == 'q':
+        break
+    given_album = input("Now give me the name of one of their albums: ")
+    if given_album == 'q':
+        break
+    cd = make_album(given_name, given_album)
+    print(cd)
+    
