ddaf872d5bcd32ebd5c3ddd7bd9976f143b4902c
learninglegion <brent.beckwith@gmail.com>
2021-04-13 23:11:12 +0000

    update2021041301

---
+++ chap9exercises.py
@@ -0,0 +1,60 @@
+# #9.1
+# class Restaurant:
+#     """An attempt to define a restuarant."""
+
+#     def __init__(self, name, cuisine):
+#         """initializing the restaurant class"""
+#         self.name = name
+#         self.cuisine = cuisine
+
+#     def describe_restaurant(self):
+#         """Describe the retaurant"""
+#         print(f"{self.name} is the epitome of {self.cuisine} food!")
+
+#     def open_restaurant(self):
+#         """Open the restaurant"""
+#         print(f"{self.name} is now open for business!")
+
+# my_restaurant = Restaurant('bojo', 'chinese')
+# print(my_restaurant.name)
+# print(my_restaurant.cuisine)
+# my_restaurant.describe_restaurant()
+# my_restaurant.open_restaurant()
+
+# #9.2.
+# s_restaurant = Restaurant('mojo', 'mexican')
+# s_restaurant.describe_restaurant()
+# d_restaurant = Restaurant('hojo', 'american')
+# d_restaurant.describe_restaurant()
+
+#9.3
+class User:
+    def __init__(self, first_name, last_name, username, password, access_level):
+        """initialize the user"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = password
+        self.access_level = access_level
+
+    def describe_user(self):
+        """describe the user"""
+        print(f"Acessing database for {self.username}:\n")
+        print(f"First name: {self.first_name.title()} Last name: {self.last_name.title()}")
+        print(f"Password: {self.password} Access level: {self.access_level}")
+        
+    def greet_user(self):
+        """Greet the user on login"""
+        print(f"Hello again, {self.username}.")
+my_user = User('brent', 'beckwith', 'bbeckwith', 'qweqwe', 'admin')
+s_user = User('stephanie', 'shelton', 'sshelton', '2wires', 'read-only')
+a_user = User('alex', 'shelton', 'ashelton', 'nspyugiogrumps', 'programmer')
+my_user.describe_user()
+my_user.greet_user()
+s_user.describe_user()
+s_user.greet_user()
+a_user.describe_user()
+a_user.greet_user()
+
+
+
