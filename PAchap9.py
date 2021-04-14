aac18d5ce9b44c9bf6dece3408643c9c11f375e6
learninglegion <brent.beckwith@gmail.com>
2021-04-14 01:24:37 +0000

    update 2021041302

--- PAchap9.py
+++ PAchap9.py
@@ -1,27 +1,73 @@
-#dog
-class Dog:
-    """A simple attempt to model a dog."""
+# #dog
+# class Dog:
+#     """A simple attempt to model a dog."""
 
-    def __init__(self, name, age):
-        """Initialize name and age attributes."""
-        self.name = name
-        self.age = age
+#     def __init__(self, name, age):
+#         """Initialize name and age attributes."""
+#         self.name = name
+#         self.age = age
 
-    def sit(self):
-        """ Simulate a dog sitting in response to a command."""
-        print(f"{self.name} is now sitting.")
+#     def sit(self):
+#         """ Simulate a dog sitting in response to a command."""
+#         print(f"{self.name} is now sitting.")
 
-    def roll_over(self):
-        """Simulate rolling over in response to a command."""
-        print(f"{self.name} rolled over!")
+#     def roll_over(self):
+#         """Simulate rolling over in response to a command."""
+#         print(f"{self.name} rolled over!")
 
-my_dog = Dog('willie', 6)
-your_dog = Dog('lucy', 3)
+# my_dog = Dog('willie', 6)
+# your_dog = Dog('lucy', 3)
 
-print(f"My dog's name is {my_dog.name}.")
-print(f"My dog is {my_dog.age} years old.")
-my_dog.sit()
+# print(f"My dog's name is {my_dog.name}.")
+# print(f"My dog is {my_dog.age} years old.")
+# my_dog.sit()
 
-print(f"Your dog's name is {your_dog.name}.")
-print(f"Your dog is {your_dog.age} years old.")
-your_dog.roll_over()
+# print(f"Your dog's name is {your_dog.name}.")
+# print(f"Your dog is {your_dog.age} years old.")
+# your_dog.roll_over()
+
+class Car:
+    """A simple attempt to represent a car."""
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odomoter_reading = 20
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"This car has {self.odomoter_reading} miles on it.")
+    
+    def update_odometer(self, mileage):
+        """Set the odomoter reading to the given value.
+        Reject the change if it attempts to roll the odometer back
+        """
+        if mileage >= self.odomoter_reading:
+            self.odomoter_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+    
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        if miles < 0:
+            print("Can't go backwards!")
+        else:
+            self.odomoter_reading += miles
+
+my_new_car = Car('audi', 'a4', 2019)
+print(my_new_car.get_descriptive_name())
+
+my_used_car = Car('toyota', 'mr2', 1986)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(-100)
+my_used_car.read_odometer()
--- chap9exercises.py
+++ chap9exercises.py
@@ -27,7 +27,81 @@
 # d_restaurant = Restaurant('hojo', 'american')
 # d_restaurant.describe_restaurant()
 
-#9.3
+# #9.3
+# class User:
+#     def __init__(self, first_name, last_name, username, password, access_level):
+#         """initialize the user"""
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.username = username
+#         self.password = password
+#         self.access_level = access_level
+
+#     def describe_user(self):
+#         """describe the user"""
+#         print(f"Acessing database for {self.username}:\n")
+#         print(f"First name: {self.first_name.title()} Last name: {self.last_name.title()}")
+#         print(f"Password: {self.password} Access level: {self.access_level}")
+        
+#     def greet_user(self):
+#         """Greet the user on login"""
+#         print(f"Hello again, {self.username}.")
+# my_user = User('brent', 'beckwith', 'bbeckwith', 'qweqwe', 'admin')
+# s_user = User('stephanie', 'shelton', 'sshelton', '2wires', 'read-only')
+# a_user = User('alex', 'shelton', 'ashelton', 'nspyugiogrumps', 'programmer')
+# my_user.describe_user()
+# my_user.greet_user()
+# s_user.describe_user()
+# s_user.greet_user()
+# a_user.describe_user()
+# a_user.greet_user()
+
+# #9.4
+# class Restaurant:
+#     """An attempt to define a restuarant."""
+
+#     def __init__(self, name, cuisine):
+#         """initializing the restaurant class"""
+#         self.name = name
+#         self.cuisine = cuisine
+#         self.number_served = 10
+
+#     def describe_restaurant(self):
+#         """Describe the retaurant"""
+#         print(f"{self.name} is the epitome of {self.cuisine} food!")
+
+#     def open_restaurant(self):
+#         """Open the restaurant"""
+#         print(f"{self.name} is now open for business!")
+
+#     def read_number_served(self):
+#         """Cite number of patrons served."""
+#         print(f"We've served {self.number_served} people!")
+    
+#     def set_number_served(self, visitors):
+#         """Set the number of people served."""
+#         if visitors >= self.number_served:
+#             self.number_served = visitors
+#         else:
+#             print("We aren't time travelers, you can't rescind your visit!")
+
+#     def increment_number_served(self, new_customers):
+#         """Increase the number of people served."""
+#         if new_customers > 0:
+#             self.number_served += new_customers
+#         else:
+#             print("You can't have negative people.")
+
+# my_restaurant = Restaurant('bojo', 'chinese')
+# my_restaurant.describe_restaurant()
+# my_restaurant.open_restaurant()
+# my_restaurant.read_number_served()
+# my_restaurant.set_number_served(50)
+# my_restaurant.read_number_served()
+# my_restaurant.increment_number_served(25)
+# my_restaurant.read_number_served()
+
+#9.5
 class User:
     def __init__(self, first_name, last_name, username, password, access_level):
         """initialize the user"""
@@ -36,6 +110,7 @@ class User:
         self.username = username
         self.password = password
         self.access_level = access_level
+        self.login_attempts = 0
 
     def describe_user(self):
         """describe the user"""
@@ -46,15 +121,29 @@ class User:
     def greet_user(self):
         """Greet the user on login"""
         print(f"Hello again, {self.username}.")
+    
+    def increment_login_attempts(self):
+        """Increase numberof login attempts"""
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        """Set login attempts back to 0"""
+        self.login_attempts = 0
+
+
 my_user = User('brent', 'beckwith', 'bbeckwith', 'qweqwe', 'admin')
-s_user = User('stephanie', 'shelton', 'sshelton', '2wires', 'read-only')
-a_user = User('alex', 'shelton', 'ashelton', 'nspyugiogrumps', 'programmer')
+# s_user = User('stephanie', 'shelton', 'sshelton', '2wires', 'read-only')
+# a_user = User('alex', 'shelton', 'ashelton', 'nspyugiogrumps', 'programmer')
 my_user.describe_user()
 my_user.greet_user()
-s_user.describe_user()
-s_user.greet_user()
-a_user.describe_user()
-a_user.greet_user()
-
-
-
+my_user.increment_login_attempts()
+my_user.increment_login_attempts()
+my_user.increment_login_attempts()
+my_user.increment_login_attempts()
+print(f"You attempted to login {my_user.login_attempts} times.")
+my_user.reset_login_attempts()
+print(f"You attempted to login {my_user.login_attempts} times.")
+# s_user.describe_user()
+# s_user.greet_user()
+# a_user.describe_user()
+# a_user.greet_user()
