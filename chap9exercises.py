66640185a3397b6c750e5327d3cc23d7ce4dc6cd
learninglegion <brent.beckwith@gmail.com>
2021-04-15 00:19:18 +0000

    update2021041401

--- chap9exercises.py
+++ chap9exercises.py
@@ -147,3 +147,155 @@
 # # s_user.greet_user()
 # # a_user.describe_user()
 # # a_user.greet_user()
+
+# #9.6 - requires restaurant class to be live
+# class IceCreamStand(Restaurant):
+#     def __init__(self, name, cuisine):
+#         """initializing the icecreamstand class"""
+#         super().__init__(name, cuisine)
+#         self.flavors = ['chocolate', 'vanilla', 'strawberry', 'rocky road']
+
+#     def display_flavors(self):
+#         print(f"We have the following flavors:")
+#         for flavor in self.flavors:
+#             print(flavor.title())
+
+# my_stand = IceCreamStand('baskin robins', 'ice cream')
+# my_stand.display_flavors()
+
+# #9.7 - requires 9.3 to be live
+# class Admin(User):
+#     def __init__(self, first_name, last_name, username, password, access_level):
+#         """initialize the admin"""
+#         super().__init__(first_name, last_name, username, password, access_level)
+#     #     self.privileges = ['can add post', 'can delete post', 'can ban user']
+#         self.privileges = Privileges()
+    
+    # def show_privileges(self):
+    #     """show the privileges an admin has"""
+    #     print(f"An admin has the following privileges:")
+    #     for privilege in self.privileges:
+    #         print(privilege)
+# my_admin = Admin('brent', 'beckwith', 'bbeckwith', 'qweqwe', 'admin')
+# my_admin.show_privileges()
+
+# #9.8 - requires 9.7 to be live but privileges commented
+# class Privileges:
+#     """Outline the privileges an admin has."""
+#     def __init__(self):
+#         self.privileges = ['can add post', 'can delete post', 'can ban user']
+
+#     def show_privileges(self):
+#         """show the privileges an admin has"""
+#         print(f"An admin has the following privileges:")
+#         for privilege in self.privileges:
+#             print(privilege)
+
+# my_admin = Admin('brent', 'beckwith', 'bbeckwith', 'qweqwe', 'admin')
+# my_admin.privileges.show_privileges()
+
+#9.9
+#electric car - inheritance
+class Car:
+    """A simple attempt to represent a car."""
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odomoter_reading = 20
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"This car has {self.odomoter_reading} miles on it.")
+    
+    def update_odometer(self, mileage):
+        """Set the odomoter reading to the given value.
+        Reject the change if it attempts to roll the odometer back
+        """
+        if mileage >= self.odomoter_reading:
+            self.odomoter_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+    
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        if miles < 0:
+            print("Can't go backwards!")
+        else:
+            self.odomoter_reading += miles
+    
+    def fill_gas_tank(self):
+        """Fill the car gas tank"""
+        long_name = f"{self.year} {self.make} {self.model}"
+        print(f"You've just filled the {long_name.title()}'s gas tank!")
+
+# my_new_car = Car('audi', 'a4', 2019)
+# print(my_new_car.get_descriptive_name())
+
+# my_used_car = Car('toyota', 'mr2', 1986)
+# print(my_used_car.get_descriptive_name())
+
+# my_used_car.update_odometer(23_500)
+# my_used_car.read_odometer()
+
+# my_used_car.increment_odometer(-100)
+# my_used_car.read_odometer()
+
+class Battery:
+    """A simple attempt to model a battery for an electric car"""
+    def __init__(self, battery_size=75):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing battery size"""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def get_range(self):
+        """Print a statement about the range this battery provides"""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+        print(f"This car can go about {range} miles on a full charge.")
+    
+    def upgrade_battery(self):
+        """upgrade battery size to 100 if it isnt already"""
+        print(f"Let's see if we can upgrade this {self.battery_size}-kWh battery.")
+        if self.battery_size < 100:
+            self.battery_size = 100
+            print(f"You were old and busted, now you have a 100-kWh battery!")
+        else:
+            print("You've got the best already, baby!")
+
+class ElectricCar(Car):
+    """Represents apects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes of PARENT class.
+        THEN initialize attributes specfic to an electric car"""
+        super().__init__(make, model, year)
+        # self.battery_size = 75 - before battery was made its own class
+        self.battery = Battery() #now the new class is called instead
+
+    # before battery was made its own class
+    # def describe_battery(self):
+    #     """Print a statement describing battery size"""
+    #     print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def fill_gas_tank(self):
+        """Fill the car gas tank"""
+        print(f"Electic cars don't have gas tanks!")
+
+my_tesla = ElectricCar('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
+my_tesla.battery.upgrade_battery()
+my_tesla.battery.get_range()
