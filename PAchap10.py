69e41d8c721819e2973506b85503856ad05243df
learninglegion <brent.beckwith@gmail.com>
2021-04-16 22:54:14 +0000

    finalupdatefortoday

--- PAchap10.py
+++ PAchap10.py
@@ -1,9 +1,9 @@
 #pi_string
-
 filename = 'pi_million_digits.txt'
 
 with open(filename) as file_object:
     lines = file_object.readlines()
+
 pi_string = ''
 for line in lines:
     pi_string += line.strip()
