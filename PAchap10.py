58fa213b59901034af07615df055a7f3654f0f75
learninglegion <brent.beckwith@gmail.com>
2021-04-19 01:42:27 +0000

    update2021041800

--- PAchap10.py
+++ PAchap10.py
@@ -1,19 +1,31 @@
-#pi_string
-filename = 'pi_million_digits.txt'
+# #pi_string
+# filename = 'pi_million_digits.txt'
 
-with open(filename) as file_object:
-    lines = file_object.readlines()
+# with open(filename) as file_object:
+#     lines = file_object.readlines()
 
-pi_string = ''
-for line in lines:
-    pi_string += line.strip()
+# pi_string = ''
+# for line in lines:
+#     pi_string += line.strip()
 
-# print(f"{pi_string[:52]}...")
-# print(len(pi_string))
+# # print(f"{pi_string[:52]}...")
+# # print(len(pi_string))
 
-#birthday in pi?
-birthday = input("Enter your birthday in the format mmddyyyy: ")
-if birthday in pi_string:
-    print ("Your birthday appears in the first million digits of pi!")
-else:
-    print("Your birthday does not appear in the first million digits of pi.")
+# #birthday in pi?
+# birthday = input("Enter your birthday in the format mmddyyyy: ")
+# if birthday in pi_string:
+#     print ("Your birthday appears in the first million digits of pi!")
+# else:
+#     print("Your birthday does not appear in the first million digits of pi.")
+
+
+#write_message
+filename = 'programming.txt'
+
+#with open(filename, 'w') as file_object:
+#    file_object.write("I love programming.\n")
+#    file_object.write("I love creating new games.\n")
+
+with open(filename, 'a') as file_object:
+    file_object.write("I also love finding meaning in large datasets.\n")
+    file_object.write("I love creating apps that can run in a browser.\n")
--- chap10exercises.py
+++ chap10exercises.py
@@ -14,14 +14,48 @@
 # for line in lines:
 #      text_string += line
 # print(text_string.rstrip())
+#
+##10.2
+#filename = 'learning_python.txt'
+#
+#with open(filename) as file_object:
+#    lines = file_object.readlines()
+#sub_string = ''
+#for line in lines:
+#    sub = line.replace('python', 'C')
+#    sub_string += sub
+#print(sub_string.rstrip())
 
-#10.2
-filename = 'learning_python.txt'
+#10.3
+#filename = 'guest.txt'
+#
+#with open(filename, 'w') as file_object:
+#    print("Please provide your name for the guestbook.")
+#    file_object.write(input("Your name: "))
 
-with open(filename) as file_object:
-    lines = file_object.readlines()
-sub_string = ''
-for line in lines:
-    sub = line.replace('python', 'C')
-    sub_string += sub
-print(sub_string.rstrip())
+##10.4
+#filename = 'guest_book.txt'
+#finished = False
+#
+#while finished == False:
+#    with open(filename, 'a') as file_object:
+#        name = input("Please provide your names for the guestbook (type 'q' when finished): ")
+#        if name == 'q':
+#            finished = True
+#            break
+#        print(f"Thanks for coming, {name}! Welcome!")
+#        name = name + '\n'
+#        file_object.write(name)
+
+#10.5.
+filename = 'programming_poll.txt'
+finished = False
+
+while finished == False:
+    with open(filename, 'a') as file_object:
+        name = input("Please enter your name for the poll ('q' if finished): ")
+        if name == 'q':
+            finished = True
+            break
+        reason = input("Why do you like programming: ")
+        file_object.write(f"{name} answered '{reason}'.\n")
